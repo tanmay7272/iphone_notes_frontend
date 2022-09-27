90f427eb282e322092bea62afc3b51830cc1b314
Tanmay Dandile <59729934+tanmay7272@users.noreply.github.com>
2022-09-27 13:36:23 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Header() {
+  return (
+    <header>
+      <h1>i Notes</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is Notes in i-notes</h1>
+      <p>Note Inside- This is Frontend of I-Notes Using react js</p>
+      <p>A mini-Project Using React</p>
+    </div>
+  );
+}
+export default Notes;
